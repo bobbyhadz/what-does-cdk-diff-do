f890fbd600493940ab8e244eef6c9da142798936
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-23 19:25:25 +0300

    added infrastructure code

---
+++ bin/cdk-starter.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import {MyCdkStack} from '../lib/cdk-starter-stack';
+
+const app = new cdk.App();
+
+new MyCdkStack(app, 'my-cdk-stack', {
+  stackName: 'my-cdk-stack',
+  env: {
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
+});
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,12 @@
+import * as s3 from '@aws-cdk/aws-s3';
+import * as cdk from '@aws-cdk/core';
+
+export class MyCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'avatars-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
